58468704802526df528d269ae4d012889308ad5f
BknDns <bkndnss@gmail.com>
2020-10-21 20:51:28 +0400

    config

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,7 @@ import Config
 
 # Личный ключ доступа к GitHub
 # Example: config :gitsome, :git_hub_key, "375cdb206cdd56392faa094cdf816ef9604bd777"
-config :gitsome, :git_hub_key, "PUT_YOUR_GITHUB_TOCKEN_HERE"
+config :gitsome, :git_hub_key, "a621ef92effc9562459494354b8a6d9b4f9214ec"
 
 # Configures the endpoint
 config :gitsome, GitsomeWeb.Endpoint,
